9927c24e96163a5dfc9c969ea1aacd408d33f6c1
bmbshlly <ritikagrawal0@gmail.com>
2025-10-19 16:40:18 +0530

    chore: userealtimesession token via api backend

--- src/components/comic/InputBar.tsx
+++ src/components/comic/InputBar.tsx
@@ -35,7 +35,6 @@ const InputBar: React.FC<InputBarProps> = ({ onGenerate, onAddMessage, disabled 
       return new OpenAI({
         dangerouslyAllowBrowser: true,
         apiKey: null,
-        // apiKey: import.meta.env.VITE_OPENAI_API_KEY,
         baseURL: 'https://api.readkraft.com/api/v1',
         });
     }, []);
