ab8a8c586ee8abb9fc09c504fc476c039d6880e5
Ritik Agrawal <ritikagrawal0@gmail.com>
2025-11-10 22:07:47 +0530

    feat: analytics (#87)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import analytics from '@/lib/analytics'
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 
 analytics.init()
-createRoot(document.getElementById("root")!).render(<App />);
+
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <SpeedInsights />
+    <Analytics />
+  </>
+);
